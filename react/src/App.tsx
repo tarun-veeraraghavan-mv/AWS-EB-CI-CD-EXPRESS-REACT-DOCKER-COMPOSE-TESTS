809a1faaff430f2387c9cec5069cffd826a7606f
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-07-15 22:46:49 -0700

    fixing react serving url6

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,19 @@
+import axios from "axios";
+
 function App() {
+  const res = async () => {
+    const response = await axios.get(
+      "http://test-express-env.eba-i2yiifzc.us-east-2.elasticbeanstalk.com/check"
+    );
+
+    console.log(response.data);
+  };
+
   return (
     <>
       <p>Hello! This React App is deployed on AWS!!</p>
       <p>Pls work!!!!!!!!</p>
+      <button onClick={res}>Get result</button>
     </>
   );
 }
